c12f8d274db879ac34affe955451ef1ec7f67006
hupeng <hupeng@adinnet.cn>
2018-08-02 15:15:56 +0800

    scheduleDemo

--- src/main/java/com/abc/icespringboot/IcespringbootApplication.java
+++ src/main/java/com/abc/icespringboot/IcespringbootApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
 @SpringBootApplication
-@MapperScan(value = "com.abc.icespringboot.mapper")
 //扫描Mapper包 为其创建代理对象
+@MapperScan(value = "com.abc.icespringboot.mapper")
+@EnableScheduling
 public class IcespringbootApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
